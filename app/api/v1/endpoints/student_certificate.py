7072ddc8244c51606f17102113c6cb4b9c9bc162
Syed Abdullah <sayed.abdullah@telus.com>
2022-11-12 19:10:51 +0530

    fixes sele query

--- app/api/v1/endpoints/student_certificate.py
+++ app/api/v1/endpoints/student_certificate.py
@@ -10,7 +10,7 @@ responseHandler = returnResponse()
 @router.get("/certificate/{student_id}/{cert_id}")
 def get_certificate_details(cert_id: int = None, student_id: int = None):
     query = f"SELECT student_certificate.id,student_certificate.quiz_id, users.name from " \
-            f"student_certificate join users on student_certificate.student_id = users.user_id where users.user_id = {student_id} and student_certificate.id = {cert_id};"
+            f"student_certificate join users on student_certificate.student_id = users.regId where users.regId = {student_id} and student_certificate.id = {cert_id};"
     m_conn = mysql_conn.mysql_obj()
     data = m_conn.mysql_execute(query, fetch_result=True)
     m_conn.close()
